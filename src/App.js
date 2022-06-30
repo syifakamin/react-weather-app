19f17bfc82db4ebc0bc5a3679b958bdf6c01825c
Amin Syifak <aminsyifak15@gmail.com>
2022-07-01 02:05:40 +0700

    Update Switching units to metrics

--- src/App.js
+++ src/App.js
@@ -34,26 +34,30 @@ function App() {
             <p>{data.name}</p>
           </div>
           <div className='temp'>
-            {data.main ? <h1>{data.main.temp} ºC</h1> : null}
+            {data.main ? <h1>{data.main.temp.toFixed()} ºC</h1> : null}
           </div>
           <div className='description'>
             {data.weather ? <p>{data.weather[0].main}</p> : null}
           </div>
         </div>
-        <div className='bottom'>
-          <div className='feels'>
-            {data.main ? <p className='bold'>{data.main.feels_like}ºC</p> : null}
-            <p>Feels Like</p>
+
+        {data.name != undefined &&
+          <div className='bottom'>
+            <div className='feels'>
+              {data.main ? <p className='bold'>{data.main.feels_like.toFixed()}ºC</p> : null}
+              <p>Feels Like</p>
+            </div>
+            <div className='humidity'>
+              {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
+              <p>Humidity</p>
+            </div>
+            <div className='wind'>
+              {data.main ? <p className='bold'>{data.wind.speed}%</p> : null}
+              <p>Wind Speed</p>
+            </div>
           </div>
-          <div className='humidity'>
-            {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
-            <p>Humidity</p>
-          </div>
-          <div className='wind'>
-            {data.main ? <p className='bold'>{data.wind.speed}%</p> : null}
-            <p>Wind Speed</p>
-          </div>
-        </div>
+        }
+
       </div>
     </div>
   );
