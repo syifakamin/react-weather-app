aa84af76515d4613b6f5132387db9628e370e1fe
Amin Syifak <aminsyifak15@gmail.com>
2022-07-01 01:42:21 +0700

    feat: add new UI and add API openweatherapp
    chore: update style index.css
    
    API fully operated in console

--- src/App.js
+++ src/App.js
@@ -1,9 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from "axios";
 
 function App() {
+
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState('');
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=9a47cf44deb33008b5456392259f0d4c`
+
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+      axios.get(url).then((Response) => {
+        setData(Response.data)
+        console.log(Response.data);
+      })
+      setLocation('')
+    }
+  }
+
   return (
     <div className="App">
-
+      <div className='search'>
+        <input
+          value={location}
+          onChange={event => setLocation(event.target.value)}
+          onKeyPress={searchLocation}
+          placeholder='Enter Location'
+          type="text" />
+      </div>
+      <div className='container'>
+        <div className='top'>
+          <div className='location'>
+            <p>Surabaya</p>
+          </div>
+          <div className='temp'>
+            <h1>60ºF</h1>
+          </div>
+          <div className='description'>
+            <p>Clouds</p>
+          </div>
+        </div>
+        <div className='bottom'>
+          <div className='feels'> <p className='bold'>65ºF</p>
+            <p>Feels Like</p>
+          </div>
+          <div className='humidity'>
+            <p className='bold'>20%</p>
+            <p>Humidity</p>
+          </div>
+          <div className='wind'>
+            <p className='bold'>12 MPH</p>
+            <p>Wind Speed</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
